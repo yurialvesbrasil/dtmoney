19f39e954b1897859bd2768d4a5857103e6ab6ad
Yuri Brasil <yurialvesbrasil@gmail.com>
2022-03-17 11:29:52 -0300

    Global Style

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { GlobalStyle } from './styles/global';
 
 const Title = styled.h1`
   color: #8257e6;
@@ -8,6 +9,7 @@ export function App() {
   return (
     <div className="App">
       <Title>Hellow Worlds</Title>
+      <GlobalStyle />
     </div>
   );
 }
